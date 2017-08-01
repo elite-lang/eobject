5c4a37089eb4a3baa04f92be715bb5ca2214188d
西风逍遥游 <sunxfancy@gmail.com>
2017-08-01 17:40:54 +0800

    add ci support

--- conanfile.py
+++ conanfile.py
@@ -3,8 +3,8 @@ from conans import ConanFile, CMake, tools
 class eobjectConan(ConanFile):
     name = "eobject"
     version = "0.1.0"
-    license = "<Put the package license here>"
-    url = "<Package recipe repository url here, for issues about the package>"
+    license = "MIT"
+    url = "https://github.com/elite-lang/eobject"
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
     default_options = "shared=False", "gtest:shared=False"
@@ -24,6 +24,7 @@ class eobjectConan(ConanFile):
         self.copy("*.so", dst="lib", keep_path=False)
         self.copy("*.dylib", dst="lib", keep_path=False)
         self.copy("*.a", dst="lib", keep_path=False)
+        self.copy("LICENSE", dst=".", keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = [self.name]
